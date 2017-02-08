f81ff0db334bd21f9652d1406c1cebb187b1b033
LawlietNick <nicknoir@gmail.com>
2017-02-08 21:24:10 +0200

    Learning git again

--- index.php
+++ index.php
@@ -3,19 +3,14 @@
 
 <head>
 	<meta charset="utf-8">
-	<title><?php bloginfo('title'); ?></title>
-  <meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<?php wp_head(); ?>
 </head>
 
 <body>
 
-<?php wp_nav_menu(array(
-    'theme_location' => 'primary'
-)); ?>
-
-Sisältö
+<?php 
+	echo '<h1>Page</h1>';
+?>
 
 </body>
 </html>
