c2fc953ea126a80ba8880600d2ab0f670be2d3aa
LawlietNick <nicknoir@gmail.com>
2017-02-18 20:42:26 +0200

    I should start from the beginning

--- index.php
+++ index.php
@@ -1,38 +1,23 @@
 <?php get_header(); ?>
 
 
-<?php 
-	if( have_posts() ):
 
-		while( have_posts() ): the_post();
-?>
+
+<?php if( have_posts() ): ?>
+<?php endif; ?>
+
+<?php /* Start the Loop */ ?>
+<main class="site-main">
+            <div class="post-body">
+<?php while ( have_posts() ) : the_post(); ?>
 			
-<div class="content-fluid">
-        <article class="post">
-            <div class="post-wrap">
-                <div class="post-header">
-                    <h2><?php the_title(); ?></h2>
-                    <p><?php the_time('j. F Y'); ?></p>
-                    <span class="cat-links">Posted in 
-                        <?php the_category(); ?>
-                    </span>
-                </div>
-                <div class="post-content inner-column centering">
-                    <?php the_content(); ?>
-                </div>
-                <!-- .post-content -->
-                <div class="post-featured-img">
-                    <img src="img/revenant_featured_img.jpeg" alt="">
-                </div>
-                <div class="tags-links">
-                    <?php the_tags(); ?>
-                </div>
-            </div>
-            <!-- .post-wrap -->
-        </article>
+<?php get_template_part( 'post-content' );?>
 
-<?php endwhile; endif;?>
+<?php endwhile; ?>
 
+
+           </div><!-- .post-body -->
+        </main>
 
 
 
