b39f0993d6a33b4384a38902fefea762e3d667b8
LawlietNick <nicknoir@gmail.com>
2017-02-22 21:41:57 +0200

    Slowly starting to add more styling, while still figuring out some of the components and how they work

--- single.php
+++ single.php
@@ -17,12 +17,14 @@ get_header(); ?>
 
 			get_template_part( 'template-parts/content', get_post_format() );
 
-			the_post_navigation();
+			/* If want to show a link to the next post
+			the_post_navigation(); */
 
-			// If comments are open or we have at least one comment, load up the comment template.
+			/* // If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
 			endif;
+			*/
 
 		endwhile; // End of the loop.
 		?>
